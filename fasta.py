9235836382867cf62f19c28ce9f1d145959ce8d8
Robin Hübner <profan@prfn.se>
2019-06-28 16:25:54 +0200

    protein junk, initial commit, fasta batch uploader and protein analysis

---
+++ fasta.py
@@ -0,0 +1,53 @@
+import os
+import csv
+import datetime
+import subprocess
+import argparse
+import shutil
+import re
+
+path_to_submit_fasta_cmd = shutil.which("svr_submit_RAST_job")
+submit_fasta_job_cmd = " --domain Bacteria --user %s --passwd %s --fasta %s"
+absolute_cmd_path = path_to_submit_fasta_cmd + submit_fasta_job_cmd
+fasta_job_output_regex = re.compile(r"'(\d+)'")
+
+parser = argparse.ArgumentParser(description='Batch upload a directory of fasta files.')
+parser.add_argument(
+    '--username', metavar='username', type=str,
+    help='your login username'
+)
+parser.add_argument(
+    '--password', metavar='password', type=str,
+    help='your login password'
+)
+parser.add_argument(
+    'directory', type=str, default=os.getcwd(), nargs="?",
+    help='the directory with the fasta (.fa) files in it to upload'
+)
+
+def write_job_id_to_file(job_id, file_name):
+    print("[batch] writing id: %s to file: %s" % (job_id, file_name))
+    if not os.path.exists('submitted_jobs.csv'):
+        with open('submitted_jobs.csv', 'w') as csvfile:
+            our_writer = csv.writer(csvfile)
+            our_writer.writerow([job_id, file_name])
+    else:
+        with open('submitted_jobs.csv', 'a') as csvfile:
+            our_writer = csv.writer(csvfile)
+            our_writer.writerow(["job_id", "fasta_file"])
+            our_writer.writerow([job_id, file_name])
+
+def submit_fasta_files_in_dir(username, password, directory):
+    for entry in os.listdir(args.directory):
+        if os.path.isfile(entry) and entry.endswith(".fa"):
+            try:
+                print("[batch] trying to submit: %s at %s" % (entry, datetime.datetime.now()))
+                output = subprocess.getoutput(absolute_cmd_path % (args.username, args.password, entry))
+                our_match = fasta_job_output_regex.search(output)
+                submitted_job_id = our_match.group(1)
+                write_job_id_to_file(submitted_job_id, entry)
+            except:
+                print("[batch] failed to submit: %s at %s" % (entry, datetime.datetime.now()))
+
+args = parser.parse_args()
+submit_fasta_files_in_dir(args.username, args.password, args.directory)
---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import csv
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculates proteins common to all genomes in directory, outputting a spreadsheet for the proteins common to all genomes and a spreadsheet which holds all the proteins unique to each genome, marked with which genome to which they are unique, if any.')
+parser.add_argument(
+    'directory', type=str, default=os.getcwd(), nargs="?",
+    help='the directory with the csv files of the genomes in it (defaults to current directory)'
+)
+
+args = parser.parse_args()
+
+# workaround
+os.chdir(args.directory)
+files = os.listdir()
+
+# first collect all proteins associated with each file
+
+data_files = []
+for entry in files:
+    filename, file_ext = os.path.splitext(entry)
+    if os.path.isfile(entry) and file_ext == '.csv':
+        with open(entry) as csvfile:
+            reader = csv.DictReader(csvfile)
+            proteins = set() # to automatically eliminate duplicates
+            for row in reader:
+                # we only care about the row if it has a figfam entry, and it is nonempty
+                if 'figfam' in row and not row['figfam'].isspace() and not row['figfam'] == "":
+                    proteins.add(row['figfam'])
+            data_files.append({'file_name' : entry, 'proteins' : proteins})
+
+# accumulate all proteins into a big list first
+all_proteins = []
+for data in data_files:
+    all_proteins.append(data['proteins'])
+
+# calculate how many unique proteins exist in total (not ones unique to each genome but overall)
+all_unique_proteins = set.union(*all_proteins)
+
+# calculate the intersection of the sets of proteins, gives us all which are common
+all_common_proteins = set.intersection(*all_proteins)
+
+print("total unique proteins: %d" % (len(all_unique_proteins)))
+print("total common proteins: %d" % (len(all_common_proteins)))
+
+# calculate and present the proteins which are unique to each genome, and to which
+unique_proteins = {}
+for e_cur in data_files:
+    cur_proteins = e_cur['proteins']
+    all_except_me = [e['proteins'] for e in data_files if e != e_cur]
+    cur_unique_proteins = cur_proteins.difference(*all_except_me)
+    unique_proteins[e_cur['file_name']] = cur_unique_proteins
+
+for (file_name, proteins) in unique_proteins.items():
+    print("file: %s has %d unique proteins" % (file_name, len(proteins)))
