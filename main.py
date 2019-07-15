2daebe8c060b129bd7374d9e70b4bc3ee0e7b7a5
Robin Hübner <profan@prfn.se>
2019-07-15 09:16:50 +0200

    write protein id also into file with unique proteins

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import csv
 import argparse
 
+# contig id may look like 58282 but also 28582_5929_etc
+def get_leading_numbers(s):
+    return s.split("_", 1)[0]
+
 parser = argparse.ArgumentParser(description='Calculates proteins common to all genomes in directory, outputting a spreadsheet for the proteins common to all genomes and a spreadsheet which holds all the proteins unique to each genome, marked with which genome to which they are unique, if any.')
 parser.add_argument(
     'directory', type=str, default=os.getcwd(), nargs="?",
@@ -17,22 +21,33 @@ files = os.listdir()
 
 # first collect all proteins associated with each file
 all_protein_data = {}
+skipped_entries = 0
+total_entries = 0
 
 data_files = []
 for entry in files:
+    have_fetched_contig_id = False
     filename, file_ext = os.path.splitext(entry)
     if os.path.isfile(entry) and file_ext == '.csv':
         with open(entry) as csvfile:
             reader = csv.DictReader(csvfile)
             proteins = set() # to automatically eliminate duplicates
+            contig_id = -1
             for row in reader:
+                # keep track of total number of entries
+                total_entries += 1
+                if not have_fetched_contig_id:
+                    have_fetched_contig_id = True
+                    contig_id = get_leading_numbers(row['contig_id'])
                 # we only care about the row if it has a figfam entry, and it is nonempty
                 if 'figfam' in row and not row['figfam'].isspace() and not row['figfam'] == "":
                     fig = row['figfam']
                     proteins.add(fig)
                     # registry of proteins in set
                     all_protein_data[fig] = {'function' : row['function']}
-            data_files.append({'file_name' : entry, 'proteins' : proteins})
+                else:
+                    skipped_entries += 1
+            data_files.append({'file_name' : entry, 'contig_id' : contig_id, 'proteins' : proteins})
 
 # check that our data is now nonempty, else there were no data files in directory
 if len(data_files) == 0:
@@ -50,6 +65,8 @@ all_unique_proteins = set.union(*all_proteins)
 # calculate the intersection of the sets of proteins, gives us all which are common
 all_common_proteins = set.intersection(*all_proteins)
 
+percentage_skipped = (skipped_entries / total_entries) * 100
+print("[protein] total skipped entries: %d (%d %% of total)" % (skipped_entries, percentage_skipped))
 print("[protein] total unique proteins: %d" % (len(all_unique_proteins)))
 print("[protein] total common proteins: %d" % (len(all_common_proteins)))
 
@@ -59,9 +76,10 @@ for e_cur in data_files:
     cur_proteins = e_cur['proteins']
     all_except_cur = [e['proteins'] for e in data_files if e != e_cur]
     cur_unique_proteins = cur_proteins.difference(*all_except_cur)
-    unique_proteins[e_cur['file_name']] = cur_unique_proteins
+    cur_contig_id = e_cur['contig_id']
+    unique_proteins[e_cur['file_name']] = (contig_id, cur_unique_proteins)
 
-for (file_name, proteins) in unique_proteins.items():
+for (file_name, (contig_id, proteins)) in unique_proteins.items():
     print("[protein] file %s has %d unique proteins" % (file_name, len(proteins)))
 
 # format and output the data
@@ -86,14 +104,15 @@ unique_file_path = os.path.join(cur_dir, 'output/unique_proteins.csv')
 with open(unique_file_path, 'w') as csvfile:
     print("[protein] wrote unique proteins to: %s", unique_file_path)
     writer = csv.DictWriter(csvfile, fieldnames=['figfam'])
-    writer = csv.DictWriter(csvfile, fieldnames=['file_name', 'figfam', 'function'])
+    writer = csv.DictWriter(csvfile, fieldnames=['file_name', 'contig_id', 'figfam', 'function'])
     writer.writeheader()
-    for (file_name, proteins) in unique_proteins.items():
+    for (file_name, (contig_id, proteins)) in unique_proteins.items():
         for fig in proteins:
             cur_fig_data = all_protein_data[fig]
             writer.writerow(
                 {
                     'file_name' : file_name,
+                    'contig_id' : contig_id,
                     'figfam' : fig,
                     'function' : cur_fig_data['function']
                 }
