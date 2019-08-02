c235558466ae37ce67360e27ff61d5efbbae1f13
Robin Hübner <profan@prfn.se>
2019-08-02 16:54:24 +0200

    update data on the fly, more or less

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    let label_font_size = 12;
+
     let margins = {
         top: 16, left: 128, right: 128, bottom: 96
     };
@@ -14,32 +16,85 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function set_of_property(arr, property) {
         return d3.set(arr.flatMap(x => x[property]).filter(x => typeof x === 'string'));
     }
+
+    let on_mouse_over = function (d) {
+        // tooltip
+        //     .style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
     
-    d3.json("data/proteins.json").then(function(data) {
+    let on_mouse_move = function (d) {
+        /*
+        tooltip
+            .html("The exact value of<br>this cell is: " + d.fig + ":" + d.contig_id)
+            .style("left", (d3.mouse(this)[0] + 70) + "px")
+            .style("top", (d3.mouse(this)[1]) + "px")
+            */
+    }
 
-        for (let key in data) {
-            data[key].fig = key;
-        }
+    let on_mouse_leave = function (d) {
+        // tooltip
+        //    .style("opacity", 0)
+        d3.select(this)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+    }
 
-        let sliced_arr = Object.values(data).sort((a, b) => d3.ascending(a.fig, b.fig)).slice(0, 50);
-        let categories = set_of_property(sliced_arr, 'category');
-        let subcategories = set_of_property(sliced_arr, 'subcategory');
-        let subsystems = set_of_property(sliced_arr, 'subsystem');
-        let roles = set_of_property(sliced_arr, 'role');
+    let protein_belongs_to_genome = function (p, contig_id) {
+        return p.contig_ids.includes(contig_id);
+    }
 
-        let genes = set_of_property(sliced_arr, 'contig_ids');
-        let figfams = set_of_property(sliced_arr, 'fig');
+    let deduplicate_data = function(slice) {
 
-        let label_font_size = 12
-        let necessary_height = figfams.size() * label_font_size
-        let necessary_width = genes.size() * label_font_size
+        let augmented_data = slice.flatMap(function(e) {
+            return e.contig_ids.map(function (c) {
+                return {
+                    fig: e.fig, contig_id: c
+                };
+            });
+        });
 
-        dims.height = necessary_height
-        dims.width = necessary_width
+        let deduplicated_data = {};
+        augmented_data.forEach(function (e) {
+            if (e.fig in deduplicated_data) {
+                deduplicated_data[e.fig].contig_ids.add(e.contig_id);
+            } else {
+                deduplicated_data[e.fig] = {
+                    fig: e.fig,
+                    contig_ids : new Set([e.contig_id])
+                };
+            }
+        });
+
+        return Object.values(deduplicated_data).flatMap(function(e) {
+            return Array.from(e.contig_ids.values()).map(function (c) {
+                return {
+                    fig: e.fig, contig_id: c
+                };
+            });
+        });
+        
+    }
+
+    function recalculate_dimensions(xs, ys) {
+
+        let necessary_height = ys.size() * label_font_size;
+        let necessary_width = xs.size() * label_font_size;
+
+        dims.height = necessary_height;
+        dims.width = necessary_width;
+
+    }
+
+    function create_from_data(array) {
+
+        let genes = set_of_property(array, 'contig_ids');
+        let figfams = set_of_property(array, 'fig');
+        recalculate_dimensions(genes, figfams);
 
         margins.left = figfams.values().reduce((a, c) => (c.length > a.length) ? c : a).length * (label_font_size*0.65);
-        let start_x = 0;
-        console.log("got start_x: " + start_x);
 
         let svg = d3.select("#data-graph")
             .append("svg")
@@ -48,15 +103,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
             .append("g")
                 .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
 
-        let genes_sorted = genes.values().sort(d3.descending)
+        let genes_sorted = genes.values().sort(d3.descending);
         let x = d3.scaleBand()
             .range([0, dims.width])
             .domain(genes_sorted)
             .padding(0.05); /* is this good? */
 
         svg.append("g")
+            .attr("class", "x axis")
             .style("font-size", label_font_size)
-            .attr("transform", "translate(" + start_x + "," + dims.height + ")")
+            .attr("transform", "translate(0," + dims.height + ")")
             .call(d3.axisBottom(x).tickSize(0))
             .selectAll("text")
                 .attr("transform", "rotate(90) translate(5, -6)") /* necessary for text to not overlap with edge of axis line */
@@ -64,15 +120,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 .attr("class", "x-axis")
             .select(".domain").remove();
         
-        let figfams_sorted = figfams.values().sort(d3.descending)
+        let figfams_sorted = figfams.values().sort(d3.descending);
         let y = d3.scaleBand()
             .range([dims.height, 0])
             .domain(figfams_sorted)
             .padding(0.05);
         
         svg.append("g")
+            .attr("class", "y axis")
             .style("font-size", label_font_size)
-            .attr("transform", "translate(" + start_x + ", 0)")
             .call(d3.axisLeft(y).tickSize(0))
             .selectAll("text")
                 .attr("class", "y-axis")
@@ -89,75 +145,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
             .style("border-radius", "5px")
             .style("padding", "5px");
 
-        // Three function that change the tooltip when user hover / move / leave a cell
-        let mouseover = function (d) {
-            tooltip
-                .style("opacity", 1)
-            d3.select(this)
-                .style("stroke", "black")
-                .style("opacity", 1)
-        }
-        
-        let mousemove = function (d) {
-            tooltip
-                .html("The exact value of<br>this cell is: " + d.fig + ":" + d.contig_id)
-                .style("left", (d3.mouse(this)[0] + 70) + "px")
-                .style("top", (d3.mouse(this)[1]) + "px")
-        }
+        let plot_data = deduplicate_data(array);
+        let plot = svg.selectAll("rect").data(plot_data);
 
-        let mouseleave = function (d) {
-            tooltip
-                .style("opacity", 0)
-            d3.select(this)
-                .style("stroke", "none")
-                .style("opacity", 0.8)
-        }
-
-        let protein_belongs_to_genome = function (p, contig_id) {
-            return p.contig_ids.includes(contig_id);
-        }
-
-        let deduplicate_data = function(slice) {
-
-            let augmented_data = slice.flatMap(function(e) {
-                return e.contig_ids.map(function (c) {
-                    return {
-                        fig: e.fig, contig_id: c
-                    };
-                });
-            });
-    
-            let deduplicated_data = {};
-            augmented_data.forEach(function (e) {
-                if (e.fig in deduplicated_data) {
-                    deduplicated_data[e.fig].contig_ids.add(e.contig_id);
-                } else {
-                    deduplicated_data[e.fig] = {
-                        fig: e.fig,
-                        contig_ids : new Set([e.contig_id])
-                    };
-                }
-            });
-    
-            return Object.values(deduplicated_data).flatMap(function(e) {
-                return Array.from(e.contig_ids.values()).map(function (c) {
-                    return {
-                        fig: e.fig, contig_id: c
-                    };
-                });
-            });
-            
-        }
-
-        let plot_data = deduplicate_data(sliced_arr);
-
+        console.log("number of genes: " + genes.size());
+        console.log("number of figfams: " + figfams.size());
         console.log("number of datapoints: " + plot_data.length);
-        console.log("necessary height: " + necessary_height);
+        console.log("necessary height: " + dims.height);
 
         // add the squares
-        svg.selectAll()
-            .data(plot_data, function (d) { return d.fig; })
-            .enter()
+        plot.enter()
             .append("rect")
                 .attr("x", function (d) { return x(d.contig_id) })
                 .attr("y", function (d) { return y(d.fig) })
@@ -169,12 +166,119 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 .style("stroke-width", 4)
                 .style("stroke", "none")
                 .style("opacity", 0.8)
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
+            .on("mouseover", on_mouse_over)
+            .on("mousemove", on_mouse_move)
+            .on("mouseleave", on_mouse_leave);
 
-        console.log("number of genes: " + genes.size());
-        console.log("number of figfams: " + figfams.size());
+        return [svg, x, y];
+
+    }
+
+    function update_from_data(svg, x, y, array) {
+
+        let genes = set_of_property(array, 'contig_ids');
+        let figfams = set_of_property(array, 'fig');
+        recalculate_dimensions(genes, figfams);
+
+        // update svg size
+        let top_level_svg = d3.select("#data-graph").select("svg");
+        top_level_svg.attr("width", dims.width + margins.left + margins.right)
+                .attr("height", dims.height + margins.top + margins.bottom)
+            .select("g")
+                .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+
+        // update axes
+
+        /*
+        let genes_sorted = genes.values().sort(d3.descending);
+        x.domain(genes_sorted)
+            .range([0, dims.width]);
+        */
+
+        svg.select(".x")
+            .attr("transform", "translate(0," + dims.height + ")")
+            .transition().duration(100)
+            .call(d3.axisBottom(x));
+
+        let figfams_sorted = figfams.values().sort(d3.descending);
+        y.domain(figfams_sorted)
+            .range([dims.height, 0]);
+        
+        svg.select(".y")
+            .transition().duration(100)
+            .call(d3.axisLeft(y));
+
+        let plot_data = deduplicate_data(array);
+        let plot = svg.selectAll("rect").data(plot_data);
+
+        // add new squares
+        plot.enter()
+            .append("rect")
+                .attr("x", function (d) { return x(d.contig_id) })
+                .attr("y", function (d) { return y(d.fig) })
+                .attr("rx", 4)
+                .attr("ry", 4)
+                .attr("width", x.bandwidth())
+                .attr("height", y.bandwidth())
+                .style("fill", function (d) { return 1; })
+                .style("stroke-width", 4)
+                .style("stroke", "none")
+                .style("opacity", 0.8)
+            .on("mouseover", on_mouse_over)
+            .on("mousemove", on_mouse_move)
+            .on("mouseleave", on_mouse_leave);
+
+        // remove old squares
+        plot.exit().remove();
+
+    }
+    
+    d3.json("data/proteins.json").then(function(data) {
+
+        for (let key in data) {
+            data[key].fig = key;
+        }
+
+        let initial_start_offset = d3.select("#data-range-start-input").property("value");
+        let initial_end_offset = d3.select("#data-range-end-input").property("value");
+        
+        let all_data_arr = Object.values(data).sort((a, b) => d3.ascending(a.fig, b.fig));
+        let sliced_arr = all_data_arr.slice(initial_start_offset, initial_end_offset);
+
+        let categories = set_of_property(all_data_arr, 'category');
+        let subcategories = set_of_property(all_data_arr, 'subcategory');
+        let subsystems = set_of_property(all_data_arr, 'subsystem');
+        let roles = set_of_property(all_data_arr, 'role');
+
+        let [svg, x, y] = create_from_data(sliced_arr);
+
+        d3.select("#data-range-start-input").on("input", function() {
+            let start_offset = +this.value;
+            let end_offset = d3.select("#data-range-end-input").property("value");
+            if (start_offset > end_offset) {
+                d3.select("#data-range-start-input").property("value", end_offset);
+            }
+            if (start_offset < 0) {
+                start_offset = this.value = 0;
+            }
+            console.log("new start: " + start_offset + " new end: " + end_offset);
+            let fresh_slice = all_data_arr.slice(start_offset, end_offset);
+            update_from_data(svg, x, y, fresh_slice);
+        });
+
+        d3.select("#data-range-end-input").on("input", function() {
+            let start_offset = d3.select("#data-range-start-input").property("value");
+            let end_offset = +this.value;
+            if (end_offset < start_offset) {
+                d3.select("#data-range-end-input").property("value", start_offset);
+            }
+            if (end_offset < 0) {
+                end_offset = this.value = 0;
+            }
+            let fresh_slice = all_data_arr.slice(start_offset, end_offset);
+            update_from_data(svg, x, y, fresh_slice);
+        });
+
         console.log("number of categories: " + categories.size());
         console.log("number of subcategories: " + subcategories.size());
         console.log("number of subsystems: " + subsystems.size());
